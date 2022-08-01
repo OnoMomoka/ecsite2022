749d5f3aae87ccd8b37997375fe03e4c02160216
OnoMomoka <92067026+OnoMomoka@users.noreply.github.com>
2022-08-01 13:15:34 +0900

    080103
    080103

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,31 @@
 const list = document.querySelectorAll("li");
 console.log(list);
+
+const removes = list[9];
+console.log(removes);
+removes.classList.add("remove");
+
+const btn = document.querySelector(".btn");
+console.log(btn);
+
+let a = 0;
+
+btn.addEventListener("click",function(){
+    if(a == 0){
+        removes.classList.remove("remove");
+        a = 1;
+    }else{
+        removes.classList.add("remove");
+        a = 0;
+    }
+})
+
+const states = document.querySelectorAll(".state");
+console.log(states);
+for(let i=0;i<states.length;i++){
+    if(states[i].innerHTML == "true"){
+        states[i].innerHTML = "販売中";
+    }else{
+        states[i].innerHTML = "売り切れ";
+    }
+}
